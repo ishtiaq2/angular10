a9e639d3ff3c0c10a9fe2d77f45dd13ef26351e6
ishtiaq2 <ishtiaq2@kth.se>
2020-07-16 21:34:54 +0200

    adds css properites, transform (translate, rotate, scale, skew) and transition

---
+++ css-transform-tranlate/src/app/css/css.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-css',
+    templateUrl: './css.component.html',
+    styleUrls: ['css.component.scss']
+})
+
+export class CSSComponent {}
